82b4855f5f7ba69de6197083e217a39461f9821e
murata0531 <coless115@gmail.com>
2021-01-06 09:00:07 +0900

    ok

--- app/src/main/java/com/example/firebase/AuthActivity.kt
+++ app/src/main/java/com/example/firebase/AuthActivity.kt
@@ -1,11 +1,32 @@
 package com.example.firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        auth = FirebaseAuth.getInstance()
+
+        val erroremail = findViewById<TextView>(R.id.erroremail)
+        val errorpass = findViewById<TextView>(R.id.errorpass)
+
+        val register = findViewById<Button>(R.id.register)
+        val tologin = findViewById<Button>(R.id.tologin)
+
+        tologin.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
